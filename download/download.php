b526bfa408f5917ed30bfb4728f500051fbc29d1
Jean Prunneaux <jean@prunneaux.com>
2017-06-21 00:03:33 +0200

    Improvements in download pages

--- download/download.php
+++ download/download.php
@@ -34,8 +34,8 @@ If not, click on <a href="<?php echo $redirect ?>">this link</a>.</p>
 <a href="#" onclick="window.history.back();" class="btn btn-default btn-lg">Return to time2backup website</a>
 <p></p>
 <p>Other downloads:
-    <a href="https://jean.prunneaux.com/files/time2backup/stable/">All files and versions</a> |
-    <a href="https://jean.prunneaux.com/files/time2backup/stable/latest/checksums.php">SHA256 checksums</a><br/>
+    <a href="index.php">All formats</a> |
+    <a href="download.php?type=checksums">SHA256 checksums</a><br/>
     <a href="https://github.com/time2backup/time2backup/blob/master/CHANGELOG.md">See changelog</a></p>
 </p>
 
--- download/index.php
+++ download/index.php
@@ -6,13 +6,13 @@
 <h2>Thank you for downloading time2backup!</h2>
 <p>
   Your download will start soon.<br/>
-  If not, click on <a href="https://jean.prunneaux.com/files/time2backup/stable/latest/download.php">this link</a>.
+  If not, click on <a href="<?php echo $redirect ?>">this link</a>.
 </p>
 <a href="#" onclick="window.history.back();" class="btn btn-default btn-lg">Return to time2backup website</a>
 <p></p>
 <p>Other downloads:
-  <a href="https://jean.prunneaux.com/files/time2backup/stable/">All files and versions</a> |
-  <a href="https://jean.prunneaux.com/files/time2backup/stable/latest/checksums.php">SHA256 checksums</a><br/>
+  <a href="index.php">All formats</a> |
+  <a href="download.php?type=checksums">SHA256 checksums</a><br/>
   <a href="https://github.com/time2backup/time2backup/blob/master/CHANGELOG.md">See changelog</a></p>
 </p>
 
